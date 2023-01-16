513bf68d70d7a29c67df948e138544b8b73d5b7a
oyenet1 <bowofadeoyerinde@gmail.com>
2023-01-16 15:50:43 +0100

    done with tidas

--- file.js
+++ file.js
@@ -4,8 +4,34 @@
 2. PascalCase
 3. snake_case
 
+Data type
+
+1. String
+2. Number(positive negative, real, floating, double)
+3. Boolean
+4. Undefined
+5. Null
+6. Object(object, array, date)
 */
 
-var my_first_name = 'Bowofade Oyerinde';
 
-console.log(my_first_name);
+
+var myFirstName = "576879"; //string
+
+var x = -8.786; //number
+let y = 10; //number
+
+
+let tomiIsTall = false; //boolean
+
+let tomiIsPrety = true; //boolean
+
+let ade; //undefined
+
+const balance = null;
+console.log(typeof balance);
+balance = 5000;
+console.log(typeof balance);
+
+// array
+let scores = [23, false, "ade", 12, 67, 29, 52, 89]; //array

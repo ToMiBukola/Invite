de5005d7e7118c9f9b4ece9e3dbd8e7ff5eb3477
oyenet1 <bowofadeoyerinde@gmail.com>
2023-02-24 18:59:57 +0100

    operators

---
+++ datatype.js
@@ -0,0 +1,22 @@
+let foods = ["Bowofade", 18, false, 456.9876, { name: "ade" }];
+
+foods[4] = "The forth elements";
+// console.log(foods[4]);
+
+const person = {
+  name: "Tomilola Fathiat",
+  age: 24,
+  isTall: true,
+  isPretty: true,
+  IsFair: false,
+  interest: ["reading", "programming", "sleeping", "eating"]
+};
+
+person["level"] = 200;
+person["department"] = "English and Literature";
+const car = { type: "Fiat", model: "500", color: "white", price: 34 };
+
+console.log(person["interest"][2]);
+
+//  creating and accessing of object
+
---
+++ operator.js
@@ -0,0 +1,71 @@
+// types of operator
+// Arithmetic Operators
+// Assignment Operators
+// Comparison Operators
+// Logical Operators
+// Conditional Operators
+// Type Operators
+
+let a = 17;
+let b = 8;
+let name = "Adeoluwa";
+
+let c = a + b; //addition operator
+let d = a - b; //subtraction operator
+let e = a * b; //multiplication operator
+let f = a / b; //disivion operator
+let g = a ** b; //power operator
+let h = a % b; //modulus operator
+
+// a++ increment
+// a-- decrement
+
+// concatenation
+
+// level of preecidence
+/*
+1. Bracket
+2. Power
+3. Division
+4. Multiplication
+5. Addition
+6. Subtraction
+
+*/
+// console.log(name + a + b);
+// console.log(a + b + name);
+
+// console.log(4 + 3 * (2 - 1));
+
+
+
+// let pow = 2 ** 3; //it means 2*2*2
+
+
+// console.log(a++);
+// console.log(a);
+
+// assignment operator
+
+/*
+
+ x = x+y  is the same as  x += y
+
+*/
+let dig = 45;
+dig += 7;
+dig -= 7;
+dig *= 7;
+dig /= 7;
+dig %= 7;
+dig **= 7;
+
+a = 7;
+b = 4;
+
+a += b;
+
+console.log(dig);
+
+
+
